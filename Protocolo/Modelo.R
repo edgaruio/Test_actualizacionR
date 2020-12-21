54a33faec3c41f7414c8c96b491fead1f589623a
Penrouse <r.penrouse@gmail.com>
2020-12-21 15:10:02 -0500

    Actualizacion 21-12-2020

--- Protocolo/Modelo.R
+++ Protocolo/Modelo.R
@@ -5,4 +5,5 @@ data <- iris
 library(writexl)
 
 write_xlsx(data, "Salidas/iris.xlsx")
+saveRDS(data, "Salidas/iris.rds")
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Test_actualizacionR
-# 21/12/2020
+# 21/12/2020 - 3:08 pm
+
